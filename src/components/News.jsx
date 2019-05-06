15f18ac5d516559b4a30b4cb973137aa8e5513e3
Joe Schmitz <josephschmitz2018@u.northwestern.edu>
2019-05-06 00:36:29 -0500

    API routing in REACT isn't easy

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react';
 // Grab the articles as a json
-import Scrape from './scrape'
+// import Scrape from './Scrape'
 
 const News = props => {
+	state = {
+		response: '',
+		post: '',
+		responseToPost: '',
+	  };
+	  
+	componentDidMount(){
+		this.callApi()
+		.then(res => this.setState({ response: res.express }))
+		.catch(err => console.log(err));
+	}
+
+	callApi = async () => {
+		const response = await fetch('/scrape');
+		const body = await response.json();
+		if (response.status !== 200) throw Error(body.message);
+		return body;
+	};
+
+
 	if (props.user) {
 		return (
 			<div className="News">
@@ -14,15 +34,25 @@ const News = props => {
 			</div>
 		)
 	} else {
+		
 		return (
-			<div className="News">
-				<p>Current User:</p>
-				<code>
-					<Scrape/>
-				</code>
+			<div className="articles">
+				<p>{this.state.response}</p>
+				<form onSubmit={this.handleSubmit}>
+				<p>
+					<strong>Post to Server:</strong>
+				</p>
+				<input
+					type="text"
+					value={this.state.post}
+					onChange={e => this.setState({ post: e.target.value })}
+				/>
+				<button type="submit">Submit</button>
+				</form>
+				<p>{this.state.responseToPost}</p>
 			</div>
+			
 		)
 	}
 }
-
-export default News
+export default News;
