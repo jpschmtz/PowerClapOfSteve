a8d511243680a8d7c4fdf0d85b58da4d483f17c5
T Saraya <saratgrffn@gmail.com>
2019-05-13 15:32:08 -0500

    form validation and creating routes

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb://Master:Password123@ds155516.mlab.com:55516/heroku_rlf28wdk"
+    mongoURI: "mongodb://Master:Password123@ds155516.mlab.com:55516/heroku_rlf28wdk",
+    secretOrKey: "secret"
+
 }
